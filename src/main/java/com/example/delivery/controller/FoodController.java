1fd88f8ea928f3db21eb747cf9f37e99e6258dcc
YMK-creator <morozovarik72@gmail.com>
2025-02-24 12:43:05 +0300

    fixed sonar

--- src/main/java/com/example/delivery/controller/FoodController.java
+++ src/main/java/com/example/delivery/controller/FoodController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/food/")
+@RequestMapping("/food")
 public class FoodController {
 
     private final InMemoryFoodServiceImpl foodService;
